6135b70a54ecf5479b91ce102b158a98f1d45954
sayrish <sayrrish1@gmail.com>
2022-11-02 21:32:05 +0530

    Handle Exaption For Invalid Mood

--- src/com/moodAnalyser/MoodAnalyzThrowable.java
+++ src/com/moodAnalyser/MoodAnalyzThrowable.java
@@ -1,17 +1,33 @@
 package com.moodAnalyser;
 
-public class MoodAnalyzer {
+public class MoodAnalyzer extends Throwable {
 	public static void welcome() {
 		System.out.println("Welcome to Mood Analyzer");
 		System.out.println("========================");
 	}
 
-	public String analyseMood(String message) {
-		if (message.contains("SAD"))
-			return "SAD";
-		else
-			return "HAPPY";
+	String message;
+
+	public MoodAnalyzer(String message) {
+		this.message = message;
 	}
 
+	public MoodAnalyzer() {
 
+	}
+
+	String MoodAnalyzer() {
+		return "SAD";
+	}
+
+	public String analyseMood(String message) throws MoodAnalyzer, MoodAnalyzerException {
+		try {
+			if (message.contains("SAD"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch (NullPointerException e) {
+			throw new MoodAnalyzerException("please enter proper message");
+		}
+	}
 }
