a26f0f4190c399769b2f866be6b097f1d15c904d
sayrish <sayrrish1@gmail.com>
2022-11-02 21:27:00 +0530

    Inform To User For Invalid Mood Sad or Happy

---
+++ src/com/moodAnalyser/MoodAnalyzThrowable.java
@@ -0,0 +1,40 @@
+package com.moodAnalyser;
+
+public class MoodAnalyzer extends Throwable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void welcome() {
+		System.out.println("Welcome to Mood Analyzer");
+		System.out.println("========================");
+	}
+
+	String message;
+
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public MoodAnalyzer() {
+
+	}
+
+	String MoodAnalyzer() {
+		return "SAD";
+	}
+
+	public String analyseMood(String message) throws MoodAnalyzer, MoodAnalyzerException {
+		try {
+			if (message.contains("SAD"))
+				return "SAD";
+			else
+				return "HAPPY";
+		} catch (NullPointerException e) {
+			throw new MoodAnalyzerException("please enter proper message");
+		}
+	}
+
+}
