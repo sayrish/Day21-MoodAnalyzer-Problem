90daf1946d3891317798b7fb4b6326dec996c592
sayrish <sayrrish1@gmail.com>
2022-11-02 21:29:37 +0530

    Ability To Check Mood

--- src/com/moodAnalyser/MoodAnalyzThrowable.java
+++ src/com/moodAnalyser/MoodAnalyzThrowable.java
@@ -1,40 +1,17 @@
 package com.moodAnalyser;
 
-public class MoodAnalyzer extends Throwable {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
+public class MoodAnalyzer {
 	public static void welcome() {
 		System.out.println("Welcome to Mood Analyzer");
 		System.out.println("========================");
 	}
 
-	String message;
-
-	public MoodAnalyzer(String message) {
-		this.message = message;
+	public String analyseMood(String message) {
+		if (message.contains("SAD"))
+			return "SAD";
+		else
+			return "HAPPY";
 	}
 
-	public MoodAnalyzer() {
-
-	}
-
-	String MoodAnalyzer() {
-		return "SAD";
-	}
-
-	public String analyseMood(String message) throws MoodAnalyzer, MoodAnalyzerException {
-		try {
-			if (message.contains("SAD"))
-				return "SAD";
-			else
-				return "HAPPY";
-		} catch (NullPointerException e) {
-			throw new MoodAnalyzerException("please enter proper message");
-		}
-	}
 
 }
